f7d36e8c12eb162c8532f815046667b3e333d7e4
Tozo sensei <codersensei@outlook.com>
2025-01-11 10:17:16 -0800

    feat(src/pages/Footer.jsx): add footer component

---
+++ client/src/pages/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+              <footer className="footer">
+        <p>&copy; 2024 H&H Salon. All rights reserved.</p>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
