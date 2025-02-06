31d83ba9e7798c83fd9bbfc9bb27f06e871f9a76
Shiro Sensei <codersensei@outlook.com>
2025-02-06 07:37:20 -0800

    feat(index): add database config and export appointment model
    - Export service routes
    - Export stylist routes
    - Configured database settings in index.ts
    - Ensured Appointment models and routes are properly exported

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,34 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './config/db';
+import appointmentRoutes from './routes/appointmentRoutes';
+import serviceRoutes from './routes/serviceRoutes';
+import stylistRoutes from './routes/stylistRoutes';
 
-const app = express();
+
+dotenv.config();
+
+
+const app: Application = express();
+
+// Connect to Database
+connectDB();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/api', (req, res) => {
+// Routes
+app.use('/api/appointments', appointmentRoutes);
+app.use('/api/services', serviceRoutes);
+app.use('/api/stylists', stylistRoutes);
+
+// Default route
+app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Hello from server!' });
 });
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
-});
+// Server
+const PORT = process.env.PORT || 8009;
+app.listen(PORT, () => console.log(`Server is running on port http://localhost:${PORT}`));
